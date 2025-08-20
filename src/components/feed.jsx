23d935816b1dcca2502c2cd877f6c112b1568f7e
Pushpendra <pushpendra616singh@gmail.com>
2025-08-20 18:29:22 +0530

    build the user card on feed

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -1,5 +1,44 @@
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"; 
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
+
+ 
 const Feed = () => {
-  return <div> Feed </div>
+
+  const feed = useSelector((store) => store.feed);
+
+  const dispatch = useDispatch();
+  const getFeed = async () => {
+    if (feed) return;
+
+    try { 
+      const res = await axios.get(BASE_URL + "/feed", { withCredentials: true ,
+
+      });
+    
+      dispatch(addFeed(res.data));
+    } catch  (err) {
+      //TODO: HAndle error
+    }
+
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    feed && (
+  
+   <div className="flex justify-center my-10">
+     <UserCard user={feed[0]} /> 
+     </div>
+    )
+  );
+
 };
 
 export default Feed;
